a34dbe4ad74252adc173970a6b6e0fdb56384d2d
Atharva Vaidya <atharva.vaidya@icloud.com>
2019-11-08 11:23:18 +0530

    Initialized project

---
+++ CRDTModel/CRDTModel.h
@@ -0,0 +1,19 @@
+//
+//  CRDTModel.h
+//  CRDTModel
+//
+//  Created by Atharva Vaidya on 11/7/19.
+//  Copyright © 2019 Atharva Vaidya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CRDTModel.
+FOUNDATION_EXPORT double CRDTModelVersionNumber;
+
+//! Project version string for CRDTModel.
+FOUNDATION_EXPORT const unsigned char CRDTModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CRDTModel/PublicHeader.h>
+
+
---
+++ CRDTModelTests/CRDTModelTests.swift
@@ -0,0 +1,23 @@
+//
+//  CRDTModelTests.swift
+//  CRDTModelTests
+//
+//  Created by Atharva Vaidya on 11/7/19.
+//  Copyright © 2019 Atharva Vaidya. All rights reserved.
+//
+
+import XCTest
+@testable import CRDTModel
+
+class CRDTModelTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+}
